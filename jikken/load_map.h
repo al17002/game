5df974441c6e834f66766e6b36648b2eccfde075
al16038 <al16038@yf069>
2019-06-11 16:12:02 +0900

    loadmapをsafeに変更

--- jikken/load_map.h
+++ jikken/load_map.h
@@ -1,24 +1,33 @@
 //load_map.h
+
+
 #ifndef LOAD_MAP_H
 #define LOAD_MAP_H
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
 void load_map(int player_id,int map[50][50]) {
 	int i=0,j=0;
 	FILE *fp;
 	char *filename = "map.txt";
 	char buf[200], *p ,*end;
 	printf("player_id:%d\n", player_id);
-	if ((fp=fopen(filename,"r"))==NULL) {
-		printf("error:E-C6-1\nマップ情報の読込に失敗しました。");
+	fopen_s(&fp, filename, "r");
+	if(fp==NULL){
+		printf("error/n");
+		//printf("error:E-C6-1\nマップ情報の読込に失敗しました。");
 		fclose(fp);
 		return;
 	}
 
 	for(i=0;i<50;i++){
 		fgets(buf,sizeof(buf),fp);
-		p=strtok(buf,",");
+		char *ctx;
+		p=strtok_s(buf,",",&ctx);
 		//printf("%s",buf);
 		//printf("%s",p);
-		while(p=strtok(NULL,",")){
+		while(p=strtok_s(NULL,",",&ctx)){
 			map[i][j]=atoi(p);
 			j++;
 		}
@@ -28,3 +37,5 @@ void load_map(int player_id,int map[50][50]) {
 }
 
 #endif
+
+ 
