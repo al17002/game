aa4528e5ae30e2c901336cf0baa62132559544dc
諒哉 花島 <r14869slb@gmail.com>
2019-06-20 17:44:28 +0900

    文字コードの変更とhitjugudgmentの修正。

--- jikken/hitJudgment.cpp
+++ jikken/hitJudgment.cpp
@@ -5,19 +5,19 @@
 
 
 
-bool hitJudgment(int direction,int enemyNUM) {
+bool hitJudgment(int direction,int enemyNUM,Enemy_t *m_Enemy) {
 	if (direction == 0){
 		//up
-		if ((player.x == m_Enemy[enemyNUM].x) && (player.y - 32 == m_Enemy[enemyNUM])) return FALSE;
+		if ((player.x == m_Enemy[enemyNUM].x) && (player.y - 32 == m_Enemy[enemyNUM].y)) return FALSE;
 	}else if (direction == 1) {
 		//left
-		if ((player.x - 32 == m_Enemy[enemyNUM].x) && (player.y == m_Enemy[enemyNUM])) return FALSE;
+		if ((player.x - 32 == m_Enemy[enemyNUM].x) && (player.y == m_Enemy[enemyNUM].y)) return FALSE;
 	}else if (direction == 2) {
 		//under
-		if ((player.x == m_Enemy[enemyNUM].x) && (player.y + 32 == m_Enemy[enemyNUM])) return FALSE;
+		if ((player.x == m_Enemy[enemyNUM].x) && (player.y + 32 == m_Enemy[enemyNUM].y)) return FALSE;
 	}else if (direction == 3) {
 		//right
-		if ((player.x + 32 == m_Enemy[enemyNUM].x) && (player.y == m_Enemy[enemyNUM])) return FALSE;
+		if ((player.x + 32 == m_Enemy[enemyNUM].x) && (player.y == m_Enemy[enemyNUM].y)) return FALSE;
 	}
 	return TRUE;
 }
