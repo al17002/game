d79d24b1faeebdb576f2428306780a71e13b22d1
諒哉 花島 <r14869slb@gmail.com>
2019-06-29 13:48:49 +0900

    pullのため

---
+++ jikken/enc_temp_folder/38df2c9c3e5a17089cd1b3d882468f3/Enemy.h
@@ -0,0 +1,33 @@
+#ifndef DEF_Enemy_H //“ñdinclude–h~
+
+#define DEF_Enemy_H
+
+#include "Savedata.h"
+
+typedef struct {
+	int num;
+	int Image[16];
+	int m_Image;
+	int y;
+	int x;
+	int muki;
+	int walking_flag;
+	item equipment;
+	bool enemy_turn;
+} Enemy_t;
+
+// ‰Šú‰»‚ğ‚·‚é
+void Enemy_Initialize(Enemy_t *Enemy,int x, int y, int *img, int num);
+
+// “®‚«‚ğŒvZ‚·‚é
+void Enemy_Update(Enemy_t *Enemy);
+
+// •`‰æ‚·‚é
+void Enemy_Draw(Enemy_t Enemy);
+
+// I—¹ˆ—‚ğ‚·‚é
+void Enemy_Finalize(Enemy_t Enemy);
+
+extern Enemy_t m_Enemy[];
+
+#endif
