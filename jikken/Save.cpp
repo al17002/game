5ce7b56dfaa1f4f4344671e2661e41d0b5ab562a
al17002 <al17002@yf060>
2019-07-08 11:34:25 +0900

    a

--- jikken/Save.cpp
+++ jikken/Save.cpp
@@ -36,23 +36,23 @@ void Save_Draw() {
 	DrawStringToHandle(550, 150, "W:セーブ遷移画面に戻る", GetColor(255, 255, 255),Font01);
 	DrawStringToHandle(30, 300, "1.アイテム情報", GetColor(255, 255, 255),Font01);//データ1アイテム情報
 	input_savedata(1);
-	DrawFormatStringToHandle(30, 400, GetColor(255, 255, 255),Font01, "装備中:%s atk:%d hp:%d", itemRef[player_item.equipment.ID].name, player_item.equipment.atk, player_item.equipment.hp);
-	DrawStringToHandle(30, 500, "所持アイテム", GetColor(255, 255, 255), Font01);
+	DrawFormatStringToHandle(30, 370, GetColor(255, 255, 255),Font01, "装備中:%s atk:%d hp:%d", itemRef[player_item.equipment.ID].name, player_item.equipment.atk, player_item.equipment.hp);
+	DrawStringToHandle(30, 440, "所持アイテム", GetColor(255, 255, 255), Font01);
 	for (i = 0; i < 10; i++) {//範囲はplayer_item.itemnum
-		DrawFormatStringToHandle(30, 550 + 40 * i, GetColor(255, 255, 255),Font01, "%d:%s atk:%d hp:%d", i, itemRef[player_item.having_item[i].ID].name, player_item.having_item[i].atk, player_item.having_item[i].hp);
+		DrawFormatStringToHandle(30, 490 + 40 * i, GetColor(255, 255, 255),Font01, "%d:%s atk:%d hp:%d", i, itemRef[player_item.having_item[i].ID].name, player_item.having_item[i].atk, player_item.having_item[i].hp);
 	}
 	DrawStringToHandle(530, 300, "2.アイテム情報", GetColor(255, 255, 255),Font01);//データ2アイテム情報
 	input_savedata(2);
-	DrawFormatStringToHandle(530, 400, GetColor(255, 255, 255),Font01, "装備中:%s atk:%d hp:%d", itemRef[player_item.equipment.ID].name, player_item.equipment.atk, player_item.equipment.hp);
-	DrawStringToHandle(530, 500, "所持アイテム", GetColor(255, 255, 255), Font01);
+	DrawFormatStringToHandle(530, 370, GetColor(255, 255, 255),Font01, "装備中:%s atk:%d hp:%d", itemRef[player_item.equipment.ID].name, player_item.equipment.atk, player_item.equipment.hp);
+	DrawStringToHandle(530, 440, "所持アイテム", GetColor(255, 255, 255), Font01);
 	for (i = 0; i < 10; i++) {
-		DrawFormatStringToHandle(530, 550 + 40 * i, GetColor(255, 255, 255),Font01, "%d:%s atk:%d hp:%d", i, itemRef[player_item.having_item[i].ID].name, player_item.having_item[i].atk, player_item.having_item[i].hp);
+		DrawFormatStringToHandle(530, 490 + 40 * i, GetColor(255, 255, 255),Font01, "%d:%s atk:%d hp:%d", i, itemRef[player_item.having_item[i].ID].name, player_item.having_item[i].atk, player_item.having_item[i].hp);
 	}
 	DrawStringToHandle(1030, 300, "3.アイテム情報", GetColor(255, 255, 255),Font01);//データ2アイテム情報
 	input_savedata(3);
-	DrawFormatStringToHandle(1030, 400, GetColor(255, 255, 255),Font01, "装備中:%s atk:%d hp:%d", itemRef[player_item.equipment.ID].name, player_item.equipment.atk, player_item.equipment.hp);
-	DrawStringToHandle(1030, 500, "所持アイテム", GetColor(255, 255, 255), Font01);
+	DrawFormatStringToHandle(1030, 370, GetColor(255, 255, 255),Font01, "装備中:%s atk:%d hp:%d", itemRef[player_item.equipment.ID].name, player_item.equipment.atk, player_item.equipment.hp);
+	DrawStringToHandle(1030, 440, "所持アイテム", GetColor(255, 255, 255), Font01);
 	for (i = 0; i < 10; i++) {
-		DrawFormatStringToHandle(1030, 550 + 40 * i, GetColor(255, 255, 255),Font01, "%d:%s atk:%d hp:%d", i, itemRef[player_item.having_item[i].ID].name, player_item.having_item[i].atk, player_item.having_item[i].hp);
+		DrawFormatStringToHandle(1030, 490 + 40 * i, GetColor(255, 255, 255),Font01, "%d:%s atk:%d hp:%d", i, itemRef[player_item.having_item[i].ID].name, player_item.having_item[i].atk, player_item.having_item[i].hp);
 	}
 }
